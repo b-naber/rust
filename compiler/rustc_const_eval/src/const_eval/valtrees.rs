b1aa14ebc24e446b8ca7c305f855d2ef2dd997fd
b-naber <bn263@gmx.de>
2022-04-14 18:00:47 +0200

    valtrees.rs changes -> squash this

--- compiler/rustc_const_eval/src/const_eval/valtrees.rs
+++ compiler/rustc_const_eval/src/const_eval/valtrees.rs
@@ -87,7 +87,7 @@ fn slice_branches<'tcx>(
 }
 
 #[instrument(skip(ecx), level = "debug")]
-pub(super) fn const_to_valtree_inner<'tcx>(
+pub(crate) fn const_to_valtree_inner<'tcx>(
     ecx: &CompileTimeEvalContext<'tcx, 'tcx>,
     place: &MPlaceTy<'tcx>,
 ) -> Option<ty::ValTree<'tcx>> {
